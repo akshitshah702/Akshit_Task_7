5d976a21821c82b2f03bd86ab103f67b40fa8d19
akshitshah702 <84558068+akshitshah702@users.noreply.github.com>
2021-06-08 20:07:57 -0700

    Add files via upload

---
+++ # Task 7 Q2.py
@@ -0,0 +1,20 @@
+# Task 7 Q2
+
+
+class Shape(object):
+    def __init__(self):
+        pass
+
+    def area(self):
+        return 0
+
+class Square(Shape):
+    def __init__(self, l):
+        Shape.__init__(self)
+        self.length = l
+
+    def area(self):
+        return self.length*self.length
+
+aSquare= Square(4)
+print aSquare.area()
